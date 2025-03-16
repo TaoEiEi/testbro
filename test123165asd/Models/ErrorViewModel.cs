7575cd0923143ad0aa809869f1162853f4c0cebb
TaoEiEi <sirichai44.ta@gmail.com>
2025-03-16 13:46:45 +0700

    153759

---
+++ test123165asd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace test123165asd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
